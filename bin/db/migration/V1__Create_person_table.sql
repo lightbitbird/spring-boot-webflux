e9846b41e071d55ec01c4937905b28bb18cb9283
seungeun kim <seung@seungeun-no-MacBook-Pro.local>
2017-07-23 19:52:38 +0900

    unnecessary files

--- bin/db/migration/V1__Create_person_table.sql
+++
@@ -1,7 +0,0 @@
-create table PERSON (
-    ID serial PRIMARY KEY,
-    FIRST_NAME varchar(100) not null,
-    LAST_NAME varchar(100) not null,
-    CREATED_DATETIME TIMESTAMP,
-    UPDATED_DATETIME TIMESTAMP
-);
